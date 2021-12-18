a7c45596db591445634c239c307e6b100c00da33
unknown <ananta.gaire123@gmail.com>
2021-12-18 17:37:51 +0545

    Login Screens  Navigation added.

--- lib/ui/pages/splash_screen/splash_screen.dart
+++ lib/ui/pages/splash_screen/splash_screen.dart
@@ -1,8 +1,26 @@
+import 'dart:async';
+
+import 'package:electronic_shop/ui/pages/login_screen/login.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  
+  
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), (){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=>LoginPage()));
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;

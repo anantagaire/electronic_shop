98667d7ba8796457cd289d291db1c2491cf528ca
unknown <ananta.gaire123@gmail.com>
2021-12-20 13:25:52 +0545

    Filter Option  added

--- lib/ui/pages/product/product_page.dart
+++ lib/ui/pages/product/product_page.dart
@@ -18,56 +18,63 @@ class ProductPage extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
 
     return SafeArea(
-      child: Consumer<ProductServices>(
-        builder: (context, state, child)
-        {
-          if(state.products==null)
-            {
-              return Center(child: CircularProgressIndicator(),);
-            }
-          else{
-            return Container(
-              padding: EdgeInsets.only(top: 20),
-              child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: width*0.4/145,
-                  ),
-                  itemCount: state.products!.data!.product!.length,
+      child: RefreshIndicator(
+        onRefresh: ()async{
+          await Provider.of<ProductServices>(context, listen: false).getProductList(0,0,[]);
 
-                  itemBuilder: (context, index){
-                    return GestureDetector(
-                      onTap: (){
-                        Navigator.of(context).push(MaterialPageRoute(builder: (_)=>ProductDetails(data: state.products!.data!.product![index])));
-                      },
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Container(
-                          padding: EdgeInsets.only(top: 10),
-                          child: Column(
-                            children: [
-                              Container(
-                                color: Colors.grey,
-                                  height: 105,
-                                  width: width*0.4,
-                                  child: Image.network(state.products!.data!.product![index].image!, height: 105, width: width*0.4, fit: BoxFit.cover,)
-                              ),
-                              SizedBox(height: 10,),
+        },
+        child: Consumer<ProductServices>(
+          builder: (context, state, child)
+          {
+            if(state.products==null)
+              {
+                print(state.products);
+                return Center(child: CircularProgressIndicator(),);
+              }
+            else{
+              return Container(
+                padding: EdgeInsets.only(top: 20),
+                child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: width*0.4/145,
+                    ),
+                    itemCount: state.products!.length,
 
-                              Text(state.products!.data!.product![index].name!, style: Theme.of(context).textTheme.bodyText2,),
-                              SizedBox(height: 10,),
-                              Text(state.products!.data!.product![index].price!, style: Theme.of(context).textTheme.caption,),
-                            ],
+                    itemBuilder: (context, index){
+                      return GestureDetector(
+                        onTap: (){
+                          Navigator.of(context).push(MaterialPageRoute(builder: (_)=>ProductDetails(data: state.products![index])));
+                        },
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Container(
+                            padding: EdgeInsets.only(top: 10),
+                            child: Column(
+                              children: [
+                                Container(
+                                  color: Colors.grey,
+                                    height: 105,
+                                    width: width*0.4,
+                                    child: Image.network(state.products![index].image!, height: 105, width: width*0.4, fit: BoxFit.cover,)
+                                ),
+                                SizedBox(height: 10,),
+
+                                Text(state.products![index].name!, style: Theme.of(context).textTheme.bodyText2,),
+                                SizedBox(height: 10,),
+                                Text(state.products![index].price!, style: Theme.of(context).textTheme.caption,),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                  }),
-            );
-          }
-        },
+                      );
+                    }),
+              );
+            }
+          },
+        ),
       )
     );
   }
